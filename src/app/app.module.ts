cdc26ab3f9804e82c0f2584878f5307f5d99443f
Albert Lourens <albert.lourens@standardbank.co.za>
2023-05-11 11:59:01 +0200

    Added salary picker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SalaryPickerComponent } from './salary-picker/salary-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalaryPickerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/salary-picker/salary-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-picker',
+  templateUrl: './salary-picker.component.html',
+  styleUrls: ['./salary-picker.component.css']
+})
+export class SalaryPickerComponent {
+
+}
