8243d9074f9fcc72d40728f262d8ab26e090aad1
Jason Davidson <jasonftw625@gmail.com>
2023-06-06 14:32:28 +0200

    update: add support for monthly amount

--- src/app/currency-input/currency-input.component.ts
+++ src/app/currency-input/currency-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-currency-input',
@@ -9,9 +10,21 @@ import { FormControl } from '@angular/forms';
 export class CurrencyInputComponent {
   @Output() valueUpdatedEvent = new EventEmitter<number>();
   amountCtrl = new FormControl(0.0, { updateOn: 'blur' });
+  monthly = false;
+  @Output() salaryIsMonthly = new EventEmitter<number>();
+
 
   updateValue(updatedVlue: Event) {
-      this.valueUpdatedEvent.emit(parseInt((updatedVlue.target as HTMLInputElement).value))
+    let val = parseInt((updatedVlue.target as HTMLInputElement).value)
+    if (this.monthly) {
+      val *= 12
+    }
+    this.valueUpdatedEvent.emit(val)
+  }
+
+  updateMonthly(value: MatCheckboxChange) {
+    this.monthly = value.checked
+    this.valueUpdatedEvent.emit((this.amountCtrl.value ?? 0) * 12)
   }
 
   ngOnInit() {
